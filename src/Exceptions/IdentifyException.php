093881fa2c75b97f23da8b7f1db04d96fe5fd71f
hussein sayed <hussein.sayed@bu.edu.eg>
2024-01-21 20:25:39 +0200

    Push

---
+++ src/Exceptions/IdentifyException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HusseinSayed\Identify\Exceptions;
+
+use Exception;
+
+/**
+ * An exception.
+ */
+class IdentifyException extends Exception
+{
+}
---
+++ tests/IdentifyServiceProviderTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace HusseinSayed\Identify\Test;
+
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+use HusseinSayed\Identify\Identify;
+use HusseinSayed\Identify\IdentifyServiceProvider;
+
+class IdentifyServiceProviderTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var MockInterface
+     */
+    protected $mockApp;
+
+    /**
+     * @var IdentifyServiceProvider
+     */
+    protected $provider;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockApp = m::mock('\Illuminate\Contracts\Foundation\Application');
+        $this->provider = new IdentifyServiceProvider($this->mockApp);
+    }
+
+    /**
+     * Test that it registers the correct service name with Identity.
+     */
+    public function testRegister()
+    {
+        $this->mockApp->shouldReceive('bind')
+            ->once()
+            ->andReturnUsing(function ($name, $closure) {
+                $this->assertEquals('laravel-identify', $name);
+                $this->assertInstanceOf(Identify::class, $closure());
+            });
+
+        $this->provider->register();
+    }
+
+    /**
+     * Test that it provides the correct service name.
+     */
+    public function testProvides()
+    {
+        $expected = ['laravel-identify'];
+        $actual = $this->provider->provides();
+
+        $this->assertEquals($expected, $actual);
+    }
+}
